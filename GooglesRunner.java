7446bb1017d1abd620f4a92dbeb82f4e84c5bb31
priyankaxworkz <priyankamogertxworkz@gmail.com>
2023-10-27 04:10:12 -0700

    add file

---
+++ GooglesRunner.java
@@ -0,0 +1,194 @@
+package com.xworkz.test.driver;
+
+import com.xworkz.test.things.Googles;
+
+public class GooglesRunner {
+	
+	public static void main(String[] args) {
+		
+		Googles googles=new Googles();
+		
+		googles.setEmailid("vgnm@dtd");
+		googles.setpassword("er5644r");
+		googles.setcontactNumber(8975653425l);
+		googles.setTotalAccount((byte)7);
+		googles.setUserName("bvyd");
+		googles.setNotification(45);
+		googles.setProfile(true);
+		googles.setFounder("larrypage,sergybrin");
+		googles.setCeo("sundar pichai");
+		googles.setFounded("4sep");
+		googles.setHeadQuaters("california");
+		googles.setCmo("lorraine twohill");
+		googles.setSubsidiaries("youtube");
+		googles.setParentOrganisationName("alphabet inc");
+		googles.setIsColor(true);
+		googles.setNumberOfBlockWebsite(3);
+		googles.setNumberOfAllowWebsite(45);
+		googles.setNumberOfContacts((short)23);
+		googles.setIsSafety(true);
+		googles.setNumberOfBussinessAccount((byte)4);
+		googles.setNumberOfPersonalAccount((byte)5);
+		googles.setStoreage(true);
+		googles.setNumberOfGooglePhotos(45);
+		googles.setNumberOfFounders(2);
+		googles.setTotalApps((short)21);
+		googles.setIsVersion(true);
+		googles.setVersionName("andriod");
+		googles.setLoginDate(4);
+		googles.setLoginTime(12);
+		googles.setEmailid1("ryud23@d");
+		googles.setPassword1("456ryte");
+		googles.setUserName1("gg3435fr");
+		googles.setPassword2("ret45etr");
+		googles.setUserName2("ryuth3");
+		googles.setUserid2("345f3");
+		googles.setContactNumber1(678553428l);
+		googles.setContactNumber2(8765432134l);
+		googles.setProfile1(false);
+		googles.setProfile1(true);
+		googles.setIsColor1(true);
+		googles.setHeadQuaters1("california");
+		googles.setCmo1("lorraine twohill");
+		googles.setSubsidiaries1("youtube");
+		googles.setParentOrganisationName1("alphabet inc");
+		googles.setLoginDate1(4);
+		googles.setLoginTime1(12);
+		
+		String emailid=googles.getEmailid();
+		System.out.println(emailid);
+		
+		String password=googles.getPassword();
+		System.out.println(password);
+		
+		long contactNumber=googles.getContactNumber();
+		System.out.println(contactNumber);
+		
+		byte totalAccount=googles.getTotalAccount();
+		System.out.println(totalAccount);
+		
+		String userName=googles.getUserName();
+		System.out.println(userName);
+		
+		int notification=googles.getNotification();
+		System.out.println(notification);
+		
+		boolean profile=googles.getProfile();
+		System.out.println(profile);
+		
+		String founder=googles.getFounder();
+		System.out.println(founder);
+		
+		String ceo=googles.getCeo();
+		System.out.println(ceo);
+		
+		String founded=googles.getFounded();
+		System.out.println(founded);
+		
+		String headQuaters=googles.getHeadQuaters();
+		System.out.println(headQuaters);
+		
+		String cmo=googles.getCmo();
+		System.out.println(cmo);
+		
+		String subsidiaries=googles.getSubsidiaries();
+		System.out.println(subsidiaries);
+		
+		String parentOrganisationName=googles.getParentOrganisationName();
+		System.out.println(parentOrganisationName);
+		
+		boolean color=googles.getIsColor();
+		System.out.println(color);
+		
+		int blockWebsite=googles.numberOfBlockWebsite();
+		System.out.println(blockWebsite);
+		
+		int allowWebsite=googles.numberOfAllowWebsite();
+		System.out.println(allowWebsite);
+		
+		short contacts=googles.numberOfContacts();
+		System.out.println(contacts);
+		
+		boolean safety=googles.getIsSafety();
+		System.out.println(safety);
+		
+		byte bussinessAccount=googles.getNumberOfBussinessAccount();
+		System.out.println(bussinessAccount);
+		
+		byte personalAccount=googles.getNumberOfPersonalAccount();
+		System.out.println(personalAccount);
+		
+		boolean storeage=googles.getStoreage();
+		System.out.println(storeage);
+		
+		int googlePhotos=googles.getNumberOfGooglePhotos();
+		System.out.println(googlePhotos);
+		
+		int numberOfFounders=googles.getNumberOfFounders();
+		System.out.println(numberOfFounders);
+		
+		short totalApps=googles.getTotalApps();
+		System.out.println(totalApps);
+		
+		boolean version=googles.getIsVersion();
+		System.out.println(version);
+		
+		String versionName=googles.getVersionName();
+		System.out.println(versionName);
+		
+		int loginDate=googles.getLoginDate();
+		System.out.println(loginDate);
+		
+		int loginTime=googles.getLoginTime();
+		System.out.println(loginTime);
+		
+		String emailid1=googles.getEmailid1();
+		System.out.println(emailid1);
+		
+		String password1=googles.getPassword1();
+		System.out.println(password1);
+		
+		String userName1=googles.getUserName1();
+		System.out.println(userName1);
+		
+		String password2=googles.getPassword2();
+		System.out.println(password2);
+		
+		String userName2=googles.getUserName2();
+		System.out.println(userName2);
+		
+		long contactNumber1=googles.getContactNumber1();
+		System.out.println(contactNumber1);
+		
+	    long contactNumber2=googles.getContactNumber2();
+		System.out.println(contactNumber2);
+		
+		boolean profile1=googles.getProfile1();
+		System.out.println(profile1);
+		
+		boolean profile2=googles.getProfile2();
+		System.out.println(profile2);
+		
+		boolean color1=googles.getIsColor1();
+		System.out.println(color1);
+		
+		String headQuaters1=googles.getHeadQuaters1();
+		System.out.println(headQuaters1);
+		
+		String cmo1=googles.getCmo1();
+		System.out.println(cmo1);
+		
+		String subsidiaries1=googles.getSubsidiaries1();
+		System.out.println(subsidiaries1);
+		
+		String parentOrganisationName1=googles.getParentOrganisationName1();
+		System.out.println(parentOrganisationName1);
+		
+		int loginDate1=googles.getLoginDate1();
+		System.out.println(loginDate1);
+		
+		int loginTime1=googles.getLoginTime1();
+		System.out.println(loginTime1);
+}
+
+}
