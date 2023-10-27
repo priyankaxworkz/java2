7e105c6f16f55938231b3af2d9649bdba9146239
priyankaxworkz <priyankamogertxworkz@gmail.com>
2023-10-27 04:01:53 -0700

    1

---
+++ Googles.java
@@ -0,0 +1,329 @@
+package com.xworkz.test.things;
+
+public class Googles {
+	
+	private String emailid;
+	private String password;
+	private long contactNumber;
+	private byte totalAccount;
+	private String userName;
+	private int notification;
+	private boolean profile;
+	private String founder;
+	private String ceo;
+	private String founded;
+	private String headQuaters;
+	private String cmo;
+	private String subsidiaries;
+	private String parentOrganisationName;
+	private boolean isColor;
+	private int numberOfBlockWebsite;
+	private int numberOfAllowWebsite;
+	private short numberOfContacts;	
+	private boolean isSafety;
+    private byte numberOfBussinessAccount;
+	private byte numberOfPersonalAccount;
+	private boolean storeage;
+	private int numberOfGooglePhotos;
+	private int numberOfFounders;
+	private short totalApps;
+	private boolean isVersion;
+	private String versionName;
+	private int loginDate;
+	private int loginTime;
+	private String emailid1;
+	private String password1;
+	private String userName1;
+	private String password2;
+	private String userName2;
+	private String userid2;
+	private long contactNumber1;
+	private long contactNumber2;
+	private boolean profile1;
+	private boolean profile2;
+	private boolean isColor1;
+	private String headQuaters1;
+	private String cmo1;
+	private String subsidiaries1;
+	private String parentOrganisationName1;
+    private int loginDate1;
+    private int loginTime1;
+	
+public void setEmailid(String emailid) {
+		this.emailid=emailid;
+	}
+public void setpassword(String password) {
+	this.password=password;
+}
+public void setcontactNumber(long contactNumber) {
+	this.contactNumber=contactNumber;
+}
+public void setTotalAccount(byte totalAccount) {
+	this.totalAccount=totalAccount;
+}
+public void setUserName(String userName) {
+	this.userName=userName;
+}
+public void setNotification(int notification) {
+	this.notification=notification;
+}
+public void setProfile(boolean profile) {
+	this.profile=profile;
+}
+public void setFounder(String founder) {
+	this.founder=founder;
+	}
+public void setCeo(String ceo) {
+	this.ceo=ceo;
+}
+public void setFounded(String founded) {
+	this.founded=founded;
+}
+public void setHeadQuaters(String headQuaters) {
+	this.headQuaters=headQuaters;
+}
+public void setCmo(String cmo) {
+	this.cmo=cmo;
+}
+public void setSubsidiaries(String subsidiaries) {
+	this.subsidiaries=subsidiaries;
+}
+public void setParentOrganisationName(String parentOrganisationName) {
+	this.parentOrganisationName=parentOrganisationName;
+}
+public void setIsColor(boolean isColor) {
+	this.isColor=isColor;
+}
+public void setNumberOfBlockWebsite( int numberOfBlockWebsite) {
+	this.numberOfBlockWebsite=numberOfBlockWebsite;
+}
+public void setNumberOfAllowWebsite(int numberOfAllowWebsite) {
+	this.numberOfAllowWebsite=numberOfAllowWebsite;
+}
+public void setNumberOfContacts(short numberOfContacts) {
+	this.numberOfContacts=numberOfContacts;
+}
+public void setIsSafety(boolean isSafety) {
+	this.isSafety=isSafety;
+}
+public void setNumberOfBussinessAccount(byte numberOfBussinessAccount) {
+	this.numberOfBussinessAccount=numberOfBussinessAccount;
+}
+public void setNumberOfPersonalAccount(byte numberOfPersonalAccount) {
+	this.numberOfPersonalAccount=numberOfPersonalAccount;
+}
+public void setStoreage(boolean storeage) {
+	this.storeage=storeage;
+}
+public void setNumberOfGooglePhotos(int numberOfGooglePhotos) {
+	this.numberOfGooglePhotos=numberOfGooglePhotos;
+}
+public void setNumberOfFounders(int numberOfFounders) {
+	this.numberOfFounders=numberOfFounders;
+}
+public void setTotalApps(short totalApps) {
+	this.totalApps=totalApps;
+}
+public void setIsVersion(boolean isVersion) {
+	this.isVersion=isVersion;
+}
+public void setVersionName(String versionName) {
+	this.versionName=versionName;
+}
+public void setLoginDate(int loginDate) {
+	this.loginDate=loginDate;
+}
+public void setLoginTime(int loginTime) {
+	this.loginTime=loginTime;
+}
+public void setEmailid1(String emailid1) {
+	this.emailid1=emailid1;
+	}
+public void setPassword1(String password1) {
+	this.password1=password1;
+}
+public void setUserName1(String userName1) {
+	this.userName1=userName1;
+}
+public void setPassword2(String password2) {
+	this.password2=password2;
+}
+public void setUserName2(String userName2) {
+	this.userName2=userName2;
+}
+public void setUserid2(String userid2) {
+	this.userid2=userid2;
+}
+public void setContactNumber1(long contactNumber1) {
+	this.contactNumber1=contactNumber1;
+}
+public void setContactNumber2(long contactNumber2) {
+	this.contactNumber2=contactNumber2;
+}
+public void setProfile1(boolean profile1) {
+	this.profile1=profile1;
+}
+public void setProfile2(boolean profile2) {
+	this.profile2=profile2;
+}
+public void setIsColor1(boolean isColor1) {
+	this.isColor1=isColor1;
+}
+public void setHeadQuaters1(String headQuaters1) {
+	this.headQuaters1=headQuaters1;
+}
+public void setCmo1(String cmo1) {
+	this.cmo1=cmo1;
+}
+public void setSubsidiaries1(String subsidiaries1) {
+	this.subsidiaries1=subsidiaries1;
+}
+public void setParentOrganisationName1(String parentOrganisationName1) {
+	this.parentOrganisationName1=parentOrganisationName1;
+}
+public void setLoginDate1(int loginDate1) {
+	this.loginDate=loginDate1;
+}
+public void setLoginTime1(int loginTime1) {
+	this.loginTime=loginTime1;
+}
+
+public String getEmailid() {
+	return this.emailid;
+}
+public String getPassword() {
+	return this.password;
+}
+public long getContactNumber() {
+	return this.contactNumber;
+}
+public byte getTotalAccount() {
+	return this.totalAccount;
+}
+public String getUserName() {
+	return this.userName;
+}
+public int getNotification() {
+	return this.notification;
+}
+public boolean getProfile() {
+	return this.profile;
+}
+public String getFounder() {
+	return this.founder;
+}
+public String getCeo() {
+	return this.ceo;
+}
+public String getFounded() {
+	return this.founded;
+}
+public String getHeadQuaters() {
+	return this.headQuaters;
+}
+public String getCmo() {
+	return this.cmo;
+}
+public String getSubsidiaries() {
+	return this.subsidiaries;
+}
+public String getParentOrganisationName() {
+	return this.parentOrganisationName;
+}
+public boolean getIsColor() {
+	return this.isColor;
+}
+public int numberOfBlockWebsite() {
+	return this.numberOfBlockWebsite;
+}
+public int numberOfAllowWebsite() {
+	return this.numberOfAllowWebsite;
+}
+public short numberOfContacts() {
+	return this.numberOfContacts;
+}
+public boolean getIsSafety() {
+	return this.isSafety;
+}
+public byte getNumberOfBussinessAccount() {
+	return this.numberOfBussinessAccount;
+}
+public byte getNumberOfPersonalAccount() {
+	return this.numberOfPersonalAccount;
+}
+public boolean getStoreage() {
+	return this.storeage;
+}
+public int getNumberOfGooglePhotos() {
+	return this.numberOfGooglePhotos;
+}
+public int getNumberOfFounders() {
+	return this.numberOfFounders;
+}
+public short getTotalApps() {
+	return this.totalApps;
+}
+public boolean getIsVersion() {
+	return this.isVersion;
+}
+public String getVersionName() {
+	return this.versionName;
+}
+public int getLoginDate() {
+	return this.loginDate;
+}
+public int getLoginTime() {
+	return this.loginTime;
+}
+public String getEmailid1() {
+	return this.emailid1;
+}
+public String getPassword1() {
+	return this.password1;
+}
+public String getUserName1() {
+	return this.userName1;
+}
+public String getPassword2() {
+	return this.password2;
+}
+public String getUserName2() {
+	return this.userName2;
+}
+public String getUserId2() {
+	return this.userid2;
+}
+public long getContactNumber1() {
+	return this.contactNumber1;
+}
+public long getContactNumber2() {
+	return this.contactNumber2;
+}
+public boolean getProfile1() {
+	return this.profile1;
+}
+public boolean getProfile2() {
+	return this.profile2;
+}
+public boolean getIsColor1() {
+	return this.isColor1;
+}
+public String getHeadQuaters1() {
+	return this.headQuaters1;
+}
+public String getCmo1() {
+	return this.cmo1;
+}
+public String getSubsidiaries1() {
+	return this.subsidiaries1;
+}
+public String getParentOrganisationName1() {
+	return this.parentOrganisationName1;
+}
+public int getLoginDate1() {
+	return this.loginDate1;
+}
+public int getLoginTime1() {
+	return this.loginTime1;
+}
+}
