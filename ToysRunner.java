a3ae9f4193d5fba280ede8700d15919ed3ae88c1
priyankaxworkz <priyankamogertxworkz@gmail.com>
2023-11-21 10:42:46 +0530

    add file

--- ToysRunner.java
+++ ToysRunner.java
@@ -1,18 +1,18 @@
-package com.xworkz.test.driver;
-import com.xworkz.test.things.Toys;
+package com.xworkz.Shopping.driver;
+
+import com.xworkz.Shopping.things.Toys;
 
 public class ToysRunner {
+	
 	public static void main(String[] args) {
-		Toys toy=new Toys();
-		System.out.println(toy);
-		ToysRunner toy1=new ToysRunner();
-		System.out.println(toy1);
-		Toys toy2=new Toys();
-		System.out.println(toy2);
-		ToysRunner toy3=new ToysRunner();
-		System.out.println(toy3);
-		System.out.println(new Toys());
-		System.out.println(new ToysRunner());
+     Toys toys = new Toys();
+     Toys toys1=new Toys();
+     System.out.println("class name is:"+toys.getClass());
+     System.out.println("String method is:"+toys.toString());
+     System.out.println("hash code is:"+toys.hashCode());
+     
+   //  System.out.println(toys.equals(toys));
+     System.out.println(toys.equals(toys1));
 	}
 
 }
