1d921cc996d27a7edb848053989f830517afdf27
priyankaxworkz <priyankamogertxworkz@gmail.com>
2023-11-21 10:40:30 +0530

    add file

--- Toys.java
+++ Toys.java
@@ -1,5 +1,27 @@
-package com.xworkz.test.things;
+package com.xworkz.Shopping.things;
 
 public class Toys {
 
+	public String toyName;
+	public int price;
+	public char size;
+	public boolean isTeddy;
+	public int type;
+	
+	@Override
+	public String toString() {
+		System.out.println("this is String method");
+		return "Toys";
+	}
+	
+	@Override
+	public int hashCode() {
+		System.out.println("this is hash code method");
+		return 250;
+		}
+	
+	public boolean equals(Object that) {
+		System.out.println("this is equals method");
+		return false;
+	}
 }
