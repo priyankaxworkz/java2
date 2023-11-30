670c7a1d22cc33326b6776b6e96e465750efaab1
priyankaxworkz <priyankamogertxworkz@gmail.com>
2023-11-30 11:54:34 +0530

    pattern programme

---
+++ HashClass.java
@@ -0,0 +1,28 @@
+package com.xworkz.interview.driver;
+
+public class HashClass {
+	
+	public static void main(String[] args) {
+		// String hash="###";
+		// String reverse="";
+		 
+		// for(int i=0;i<hash.length();i++) {
+			// reverse=hash.charAt(i)+reverse;
+			// System.out.println(reverse);
+		// }
+		
+		 
+		int row=2;
+			for(int i=row;i>=0;--i) {
+			for(int j=0;j<=i;++j) {
+					
+				
+					System.out.print("#");
+					
+			}	
+		
+				System.out.println();
+		
+	}
+
+	}}
---
+++ Star.java
@@ -0,0 +1,24 @@
+package com.xworkz.interview.driver;
+
+public class Star {
+	
+	public static void main(String[] args) {
+		
+		String name="###";
+		//String reverse="";
+		
+		for(int i=name.length()-1;i>=0;i--) {
+			//reverse=reverse+name.charAt(i);
+			
+			System.out.println(name);
+	}
+		
+		//int number=4;
+	//	for(int i=0;i<number;i++) {
+		//	for(int j=0;j<=i;j++) {
+			//	System.out.print("#");
+			//}
+		//	System.out.println();
+		//}
+		}
+}
